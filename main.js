c13696540b88b21be389b39a3dade4f4925bc4e4
Sean Coleman <seancoleman@gmail.com>
2018-10-11 05:49:08 +1100

    first

--- main.js
+++ main.js
@@ -1,54 +1,62 @@
 const env = new environment();
-const snake = [];
-addSegment(300,300);
+const snake = new snake;
+const foods = [];
 
-function environment(){
+addSegment(300, 300);
+addFood(100, 100);
+addFood(200, 200);
+addFood(400, 400);
+
+function environment() {
   this.xBoard = 600;
   this.yBoard = 600;
-  this.jump =10;
+  this.jump = 10;
 }
-
-function segment(x,y){
-  this.x = x;
-  this.y = y;
-}
-
-function addSegment(x,y){
-  var seg = new segment(x,y);
-  snake.push(seg);
-}
-
-
-function move(e){
-  plotPos(e);
+function snake(){
+  const segments = [];
   
-  //console.log(snake);
-  renderSnake();  
+  function addSegment(x, y) {
+    const seg = document.createElement('div');
+    seg.classList.add('player');
+    if(snake.length < 1){
+      seg.top = '700px';
+      seg.left = '700px';
+    }
+    const board = document.querySelector('.board');
+    board.appendChild(seg);
+    this.segments.push(seg);
+  }
+
+  
+function move(e) {
+  plotPos(e);
+  renderSnake();
+  checkCollision();
 }
 
-function plotPos(e){
+function plotPos(e) {
   const key = e.keyCode;
-  switch(key){
-    case 87:  //Up
-      if(snake[0].y >= (0 + env.jump)){
+  switch (key) {
+    case 87: //Up
+      if (snake[0].y >= 0 + env.jump) {
         snake[0].y -= env.jump;
       }
       break;
 
-    case 83:  //Down
-      if(snake[0].y < (env.yBoard - env.jump)){
+    case 83: //Down
+      if (snake[0].y < env.yBoard - env.jump) {
         snake[0].y += env.jump;
       }
       break;
 
-    case 65:  //Left
-      if(snake[0].x >= (0 + env.jump)){
+    case 65: //Left
+      if (snake[0].x >= 0 + env.jump) {
         snake[0].x -= env.jump;
       }
       break;
 
-    case 68:  //Right
-      if(snake[0].x < (env.xBoard - env.jump)){
+    case 68: //Right
+      if (snake[0].x < env.xBoard - env.jump) {
         snake[0].x += env.jump;
       }
       break;
@@ -57,11 +65,46 @@ function plotPos(e){
       break;
   }
 }
-
-function renderSnake(){
-  document.querySelector('.player').style.top = snake[0].y + 'px';
-  document.querySelector('.player').style.left = snake[0].x + 'px';
-  console.log(snake[0]);
 }
 
-window.addEventListener('keydown', move);
+
+function food(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function addFood(x, y) {
+  var a = new food(x, y);
+  foods.push(a);
+}
+
+
+function checkCollision() {
+  foods.forEach(function(e, i) {
+    if (snake[0].x == e.x && snake[0].y == e.y) {
+      console.log("The foods is gones");
+      console.log(foods);
+      console.log(e);
+      console.log(i);
+      eat(i);
+    }
+  });
+}
+
+function eat(i) {
+  foods.splice(i, 1);
+  addFood(600,100);
+}
+
+function renderSnake() {
+  document.querySelector(".player").style.top = `${snake[0].y}px`;
+  document.querySelector(".player").style.left = snake[0].x + "px";
+  //.log(snake[0]);
+  //console.log(foods);
+}
+
+function renderFoods() {
+  
+}
+
+window.addEventListener("keydown", move);
