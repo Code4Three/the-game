77c5a238ae65a26050193829fb37762187bb06c4
Sean Coleman <seancoleman@gmail.com>
2018-10-21 08:52:07 +1100

    Added Play function to control game flow. Started stripping functionality like check if food eaten out of snake.move into there own functions.

--- main.js
+++ main.js
@@ -1,96 +1,111 @@
-const options = {
-  //Game
-  mult: 0.95, //how much to decrease speed interval each grow
-  //board
-  xBoard: 300,
-  yBoard: 300,
-  //Snake
-  xStart: 150,
-  yStart: 150,
-  jump: 10,
-  speed: 1000,
-  maxSpeed: 100,
-  //Food
-  foodTime: 10000,
-  foodMult: 0.5,
-  maxFoods: 2,
-  maxMaxFoods: 10
-};
+class Game {
+  constructor() {
+    //Game
+    this.mult = 0.95; //how much to decrease speed interval each grow
+    //board
+    this.xBoard = 300;
+    this.yBoard = 300;
+    //Snake
+    this.xStart = 150;
+    this.yStart = 150;
+    this.jump = 10;
+    this.speed = 400;
+    this.maxSpeed = 100;
+    //Food
+    this.foodTime = 10000;
+    this.foodMult = 0.5;
+    this.maxFoods = 2;
+    this.maxMaxFoods = 10;
+  }
 
-function setup() {
-  //Create Board Element
-  const body = document.getElementsByTagName("body")[0];
-  const board = document.createElement("div");
-  body.appendChild(board);
-  board.classList.add("board");
-  board.style.width = `${options.xBoard}px`;
-  board.style.height = `${options.yBoard}px`;
+  setup(snake) {
+    //Create Board Element
+    const body = document.getElementsByTagName('body')[0];
+    const board = document.createElement('div');
+    body.appendChild(board);
+    board.classList.add('board');
+    board.style.width = `${this.xBoard}px`;
+    board.style.height = `${this.yBoard}px`;
 
-  //Create Snakes head
-  const head = board.appendChild(document.createElement("div"));
-  head.classList.add("head");
-  head.style.top = `${options.yStart}px`;
-  head.style.left = `${options.xStart}px`;
-  snake1.body.push(head);
+    //Create Snakes head
+    const head = board.appendChild(document.createElement('div'));
+    head.classList.add('head');
+    head.style.top = `${this.yStart}px`;
+    head.style.left = `${this.xStart}px`;
+    snake.body.push(head);
 
-  //create gameover div
-  const gameOver = board.appendChild(document.createElement("div"));
-  gameOver.classList.add("gameOver");
-  gameOver.style.visibility = "hidden";
-  gameOver.innerHTML = "GAME OVER";
+    //create popup div
+    const popup = board.appendChild(document.createElement('div'));
+    popup.classList.add('popup');
+    popup.style.visibility = 'hidden';
+    popup.innerHTML = 'GAME OVER';
+  }
+
+  checkgameOver(snake) {
+    snake.body.forEach(bodyPart => {
+      if (bodyPart.className != 'head') {
+        if (
+          snake.body[0].style.top == bodyPart.style.top &&
+          snake.body[0].style.left == bodyPart.style.left
+        ) {
+          document.querySelector('.gameOver').style.visibility = 'visible';
+          clearInterval(foodTimer);
+        }
+      }
+    });
+  }
 }
 
 /***************************SNAKE**********************/
-class snake {
-  constructor() {
+class Snake {
+  constructor(direction) {
     this.body = [];
-    this.direction = "w"; //maybe randomise this
+    this.direction = 'w'; //maybe randomise this
+    this.hungry = true;
   }
 
-  move() {
-    const head = document.querySelector(".head");
+  move(game, foods) {
+    const head = document.querySelector('.head');
     let x = parseInt(head.style.left);
     let y = parseInt(head.style.top);
-    let xLeader = x;
-    let yLeader = y;
 
     switch (this.direction) {
       //Up
-      case "w":
+      case 'w':
         if (y == 0) {
-          y = options.yBoard - options.jump;
+          y = game.yBoard - game.jump;
         } else {
-          y -= options.jump;
+          y -= game.jump;
         }
         head.style.top = `${y}px`; //move head
         break;
 
       //Down
-      case "s":
-        if (y == options.yBoard - options.jump) {
+      case 's':
+        if (y == game.yBoard - game.jump) {
           y = 0;
         } else {
-          y += options.jump;
+          y += game.jump;
         }
         head.style.top = `${y}px`; //move head
         break;
 
       //Left
-      case "a":
+      case 'a':
         if (x == 0) {
-          x = options.xBoard - options.jump;
+          x = game.xBoard - game.jump;
         } else {
-          x -= options.jump;
+          x -= game.jump;
         }
         head.style.left = `${x}px`; //move head
         break;
 
       //Right
-      case "d":
-        if (x == options.xBoard - options.jump) {
+      case 'd':
+        if (x == game.xBoard - game.jump) {
           x = 0;
         } else {
-          x += options.jump;
+          x += game.jump;
         }
         head.style.left = `${x}px`; //move head
         break;
@@ -98,21 +113,17 @@ class snake {
       default:
         break;
     }
+  }
 
-    const gameOver = this.checkBumpedHead();
-    if (!gameOver) {
-      const moveTimer = window.setTimeout(
-        () => snake1.move.call(snake1),
-        options.speed
-      );
-    } else {
-      document.querySelector(".gameOver").style.visibility = "visible";
-      clearInterval(foodTimer);
-    }
-
+  moveBody() {
+    const head = document.querySelector('.head');
+    let x = parseInt(head.style.left);
+    let y = parseInt(head.style.top);
+    let xLeader = x;
+    let yLeader = y;
     //move body
     this.body.forEach(body => {
-      if (body.className == "head") {
+      if (body.className == 'head') {
         body.style.top = `${y}px`;
         body.style.left = `${x}px`;
       } else {
@@ -124,126 +135,126 @@ class snake {
         yLeader = y;
       }
     });
-
-    this.checkDinnerTime();
   }
 
-  checkDinnerTime() {
-    const board = document.querySelector(".board");
+  checkDinnerTime(foods) {
+    const board = document.querySelector('.board');
     const food = foods.food;
     const xHead = this.body[0].style.left;
     const yHead = this.body[0].style.top;
-    food.forEach((food1, index) => {
-      if (xHead == food1.style.left && yHead == food1.style.top) {
+    food.forEach((f, index) => {
+      if (xHead == f.style.left && yHead == f.style.top) {
         food.splice(index, 1);
-        board.removeChild(food1);
-        this.grow();
-        foods.addFood();
+        board.removeChild(f);
+        this.hungry = false;
       }
     });
   }
 
-  checkBumpedHead() {
-    const gameOver = 0;
-
-    this.body.forEach(body => {
-      if (body.className != "head") {
-        if (
-          this.body[0].style.top == body.style.top &&
-          this.body[0].style.left == body.style.left
-        ) {
-          this.gameOver = 1;
-        }
-      }
-    });
-    return this.gameOver;
-  }
-
-  grow() {
+  grow(game) {
+    console.log(this.hungry);
     //get head
-    const head = document.querySelector(".head");
+    const head = document.querySelector('.head');
     //attach head to body
     if (!this.body.length) {
       this.body.push(head);
     }
     //create new body part
     const newBody = document
-      .querySelector(".board")
-      .appendChild(document.createElement("div"));
-    newBody.classList.add("body");
+      .querySelector('.board')
+      .appendChild(document.createElement('div'));
+    newBody.classList.add('body');
     newBody.style.top = head.style.top;
     newBody.style.left = head.style.left;
 
     this.body.push(newBody); //attach new body part
 
-    if (options.speed > options.maxSpeed) {
-      options.speed *= options.mult; //alter move interval (speed)
+    if (game.speed > game.maxSpeed) {
+      game.speed *= game.mult; //alter move interval (speed)
     }
 
-    options.foodTime *= options.mult;
+    game.foodTime *= game.mult;
 
-    if (options.maxFoods < options.maxMaxFoods) {
-      options.maxFoods += options.foodMult;
+    if (game.maxFoods < game.maxMaxFoods) {
+      game.maxFoods += game.foodMult;
     }
+    this.hungry = true;
   }
 
   steer(e) {
     switch (e.keyCode) {
       case 87: //Up
-        snake1.direction = "w";
+        return 'w';
         break;
 
       case 83: //Down
-        snake1.direction = "s";
+        return 's';
         break;
 
       case 65: //Left
-        snake1.direction = "a";
+        return 'a';
         break;
 
       case 68: //Right
-        snake1.direction = "d";
+        return 'd';
         break;
 
       default:
         break;
     }
-    snake1.move;
   }
 }
 
 /**********************FOOD**************/
-class food {
+class Food {
   constructor() {
     this.food = [];
   }
 
-  addFood() {
-    if (this.food.length > options.maxFoods - 1) {
+  addFood(game) {
+    if (this.food.length > game.maxFoods - 1) {
       return;
     }
-    const board = document.querySelector(".board");
-    const xRand = Math.floor(Math.random() * (options.xBoard / 10)) * 10;
-    const yRand = Math.floor(Math.random() * (options.yBoard / 10)) * 10;
-    const newFood = document.createElement("div");
+    const board = document.querySelector('.board');
+    const xRand = Math.floor(Math.random() * (game.xBoard / 10)) * 10;
+    const yRand = Math.floor(Math.random() * (game.yBoard / 10)) * 10;
+    const newFood = document.createElement('div');
 
     board.appendChild(newFood);
-    newFood.classList.add("food");
+    newFood.classList.add('food');
     newFood.style.left = `${xRand}px`;
     newFood.style.top = `${yRand}px`;
     this.food.push(newFood);
   }
 }
 
-const snake1 = new snake();
-const foods = new food();
-const foodTimer = window.setInterval(
-  () => foods.addFood.call(foods),
-  options.foodTime
-);
+(function play() {
+  const game = new Game();
+  const snake = new Snake();
+  const foods = new Food();
+  const setup = game.setup(snake);
 
-setup();
-foods.addFood();
-snake1.move();
+  const foodTimer = window.setInterval(
+    () => foods.addFood(game),
+    game.foodTime
+  );
+  foods.addFood(game);
 
-window.addEventListener("keydown", snake1.steer);
+  const snakeTimer = window.setInterval(function() {
+    //const eaten = snake.checkDinnerTime(foods);
+    //var e = 10;
+    if (!snake.hungry) {
+      snake.grow(game);
+      foods.addFood(game);
+    }
+
+    snake.moveBody();
+    snake.move(game, foods);
+    snake.checkDinnerTime(foods);
+  }, game.speed);
+
+  //snake.move(game, foods);
+  window.addEventListener('keydown', e => {
+    snake.direction = snake.steer(e);
+  });
+})();
